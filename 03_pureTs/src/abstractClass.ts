037f5bd0dd1941aa4beb907ae3833b86d9a14062
Himanshu Maurya <mauryahimanshu979@gmail.com>
2025-10-26 19:34:25 +0530

    feat : Abstract Class

--- 03_pureTs/src/abstractClass.ts
+++ 03_pureTs/src/abstractClass.ts
@@ -1,31 +1,32 @@
 abstract class TakePhoto {
     constructor(
-        public cameraMode:string,
-        public filter:string
-    ){}
-   abstract getSepia():void
-   getReelTime():number{
-    // some complex calculation
-    return 8
-   }
+        public cameraMode: string,
+        public filter: string
+    ) { }
+    abstract getSepia(): void
+    getReelTime(): number {
+        // some complex calculation
+        return 8
+    }
 }
 
-class Instagram extends TakePhoto{
+class InstagramApp extends TakePhoto {
     constructor(
-        public cameraMode:string,
-        public filter:string,
-        public burst:number
-    ){
-        super(cameraMode,filter)
+        public cameraMode: string,
+        public filter: string,
+        public burst: number
+    ) {
+        super(cameraMode, filter)
     }
 
-    getSepia():void {
+    getSepia(): void {
         console.log("Sepia");
     }
 }
 
 // const hc = new TakePhoto("test","Test") // Not valid
 
-// const hc = new Instagram("test","Test")
+// const hc = new InstagramApp("test","Test")
 
-const hc = new Instagram("test","Test",3)
+const hc = new InstagramApp("test", "Test", 3)
+hc.getReelTime()
