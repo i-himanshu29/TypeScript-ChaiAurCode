a9ce8122020351eda3132a37aa44c7fe43090eef
Himanshu Maurya <mauryahimanshu979@gmail.com>
2025-10-26 22:04:11 +0530

    generics ,generic with fn and class type generic

--- 03_pureTs/src/myGenerics.ts
+++ 03_pureTs/src/myGenerics.ts
@@ -1,34 +1,37 @@
-const score : Array<number> = []
+const score: Array<number> = []
 
-const names:Array<string> =[]
+const names: Array<string> = []
 
-function identityOne(val:boolean | number) : boolean | number{
+function identityOne(val: boolean | number): boolean | number {
     return val
 }
 
-function identityTwo(val:any):any{
+function identityTwo(val: any): any {
     return val
 }
 
-function identityThree<Type>(val : Type):Type{
+function identityThree<Type>(val: Type): Type {
     return val
 }
 
+// identityThree(3)
 // identityThree("3")
+// identityThree(true)
 
-function identityFour<T>(val:T):T{
+function identityFour<T>(val: T): T {
     return val
 }
 
-interface Bootle{
-    brand:string,
-    type:number
-
+interface Bottle {
+    brand: string,
+    type: number
 }
 
-// identityFour<Bootle>({})
+// identityFour<Bottle>({})
 
-function getSearchProducts<T>(products:T[]):T{
+
+
+function getSearchProducts<T>(products: T[]): T {
     // do some database operations
     const myIndex = 3
     return products[3]
@@ -37,20 +40,20 @@ function getSearchProducts<T>(products:T[]):T{
 // arrow function
 // sometime <T> is like as <T,> which don't give us error  becoz it shows 
 //<T> is not a HTML tag or jsx syntax but it is generic
-const getMoreSearchProducts = <T>(products:T[]): T =>{ // generic arrow method,reference,data type, arrow function and definition
+const getMoreSearchProducts = <T>(products: T[]): T => { // generic arrow method,reference,data type, arrow function and definition
     //do some database operations
     const myIndex = 4
     return products[myIndex]
 }
 
-function anotherFunction<T,U>(valOne:T,valTwo:U):object{
+function anotherFunction<T, U>(valOne: T, valTwo: U): object { // <T, U> -> that , we can use multiple generics
     return {
         valOne,
         valTwo
     }
 }
 
-anotherFunction(3,"4")
+anotherFunction(3, "4")
 
 
 
@@ -62,36 +65,36 @@ anotherFunction(3,"4")
 // }
 // anotherFunction1(3,"4")
 
-interface Database {
-    connection:string,
-    username:string,
-    password:string
+interface Database { // generic constraint
+    connection: string,
+    username: string,
+    password: string
 }
 
-    function anotherFunction1<T,U extends Database>(valOne:T,valTwo:U):object{
-        return {
-            valOne,
-            valTwo
-        }
+function anotherFunction1<T, U extends Database>(valOne: T, valTwo: U): object {
+    return {
+        valOne,
+        valTwo
     }
-    anotherFunction(3,{})
+}
+anotherFunction(3, {}) 
 
-interface Quiz{
-    name:string,
-    type:string
+interface Quiz { // generic interface
+    name: string,
+    type: string
 }
 
-interface Course{
-    name:string,
-    author:string,
-    subject:string
+interface Course { // generic interface
+    name: string,
+    author: string,
+    subject: string
 }
 
-class Sellable<T,>{
-    public cart:T[]=[]
+class Sellable<T,>{ //generic class
+    public cart: T[] = [] //generic array
 
-    addToCart(products:T){
-        this.cart.push(products)
+    addToCart(products: T) { //generic method
+        this.cart.push(products) // add products to cart array
     }
 }
 
