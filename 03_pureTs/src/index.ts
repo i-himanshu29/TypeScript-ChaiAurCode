b4643722e98d256778a1461b7330e61ebbc07dfb
Himanshu Maurya <mauryahimanshu979@gmail.com>
2025-10-26 19:13:05 +0530

    Pure ts includes Class,Constructor,access modifier

--- 03_pureTs/src/index.ts
+++ 03_pureTs/src/index.ts
@@ -2,73 +2,75 @@ console.log("typescript is here still")
 console.log("typescript is amazing")
 
 //create classes in typescript
-// class User {
-//     email:string
-//     name:string
-//     readonly city:string="Jaipur"
-//     constructor(email:string,name:string){
-//         this.email = email;
-//         this.name = name
-//     }
-// }
+class User {
+    email: string
+    name: string
+    readonly city: string = "Jaipur"
+    constructor(email: string, name: string) {
+        this.email = email;
+        this.name = name
+    }
+}
 
-// const himanshu = new User("h@h.com","himanshu")
-
+const himanshu = new User("h@h.com", "himanshu")
 // himanshu.city 
 
 
 // if you can private it you cannot access city 
 
 // Everything you don't mark are as public and or as private
-class User {
-    public email:string
-    name:string // you can use # in the place of private like '#name' over 'private name'
+class UserDetails {
+    public email: string
+    private name: string // you can use # in the place of private like '#name' over 'private name'
     // # is javascript world and private is typescript world
-    private readonly city:string="Jaipur"
-    constructor(email:string,name:string){
+    private readonly city: string = "Amritsar"
+    constructor(email: string, name: string) {
         this.email = email;
         this.name = name
     }
 }
 
-const himanshu = new User("h@h.com","himanshu")
+const himanshuJi = new UserDetails("h@h.com", "himanshu")
 
-himanshu.city 
+himanshuJi.email // accessiable
+// himanshuJi.name // not accessiable
 
 
 //************************************* */
 
-class UserA {
+class UserHaiJi {
 
-    // private _courseCount=1
-    protected _courseCount=1
+    // private _courseCount = 1
+    protected _courseCount = 1
 
 
-    readonly city:string = "jaipur"
+    readonly city: string = "jaipur"
     constructor(
-        public email:string,
-        public name:string,
+        public email: string,
+        public name: string,
         // private userId:string
-        ){
+    ) {
 
     }
 
     // similar to private property there is private method as well
-    private deleteToken(){
+    private deleteToken() {
         console.log("Token deleted");
     }
 
-    get getAppleEmail():string{
+    // getter have return type
+    get getAppleEmail(): string {
         return `apple${this.email}`
     }
 
-    get courseCount():number {
+    // getter for course count
+    get courseCount(): number {
         return this._courseCount
     }
 
     // setter have no return type
     set courseCount(courseNum) { // you are not allowed to write number or void
-        if(courseNum <= 1){
+        if (courseNum <= 1) {
             throw new Error("Course")
         }
         this._courseCount = courseNum
@@ -76,15 +78,16 @@ class UserA {
 
 }
 
-const hitesh = new UserA("h@h.com","hitesh")
+const hitesh = new UserHaiJi("h@h.com", "hitesh")
 
-hitesh.deleteToken() // not accessiable becoz out of scope
+// hitesh.deleteToken() // not accessiable becoz out of scope
 
 //--------------------------------------------
-
-class subUser extends User {
-    isFamily : boolean = true
-    changeCourseCount(){
+// Protected can be used in same class a well as in the child class
+// Private can only be used in the same class
+class subUser extends UserHaiJi { // it cannot acquire a private property
+    isFamily: boolean = true
+    changeCourseCount() {
         this._courseCount = 4;
     }
 }
