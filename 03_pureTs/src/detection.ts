152320275f1096f5b463d3675f28c2a74466ad73
Himanshu Maurya <mauryahimanshu979@gmail.com>
2025-10-26 22:38:23 +0530

    Narrowing,in operator,instanceOf,type Predicate, Discriminated Unions ,

--- 03_pureTs/src/detection.ts
+++ 03_pureTs/src/detection.ts
@@ -1,122 +1,125 @@
-function detectType(val:number | string){
-    if(typeof val === "string"){
-      return val.toLowerCase()
-    }
-    return val + 3
+// type Narrowing = number | string
+function detectType(val: number | string) {
+  if (typeof val === "string") {
+    return val.toLowerCase()
+  }
+  return val + 3
 }
 
-// extra focus needed
-function provideId(id:string | null){
-    if(!id){
-        console.log("please provide ID");
-        return
-    }
-    id.toLowerCase()
+// extra focus needed 
+function provideId(id: string | null) {
+  if (!id) {
+    console.log("please provide ID");
+    return
+  }
+  id.toLowerCase()
 }
 
 
-  function printAll(strs: string | string[] | null) {
-    
-    if (strs) {
-      if (typeof strs === "object") { // array also treated as object
-        for (const s of strs) {
-          console.log(s);
-        }
-      } else if (typeof strs === "string") {
-        console.log(strs);
+function printAll(strs: string | string[] | null) {
+
+  if (strs) {
+    if (typeof strs === "object") { // array also treated as object
+      for (const s of strs) {
+        console.log(s);
       }
+    } else if (typeof strs === "string") {
+      console.log(strs);
     }
   }
+}
 
- //---------------------------------
+//---------------------------------
 
-  interface User {
-    name:string,
-    email:string
+interface User {
+  name: string,
+  email: string
+}
+
+interface Admin {
+  name: string,
+  email: string,
+  isAdmin: boolean // isAdmin only exists when interface is Admin
+}
+
+function isAdminAccount(account: User | Admin) {
+  if ("isAdmin" in account) {
+    return account.isAdmin
   }
-
-  interface Admin{
-    name:string,
-    email:string,
-    isAdmin:boolean // isAdmin only exists when interface is Admin
-  }
-
-  function isAdminAccount(account:User | Admin){
-    if("isAdmin" in account){
-        return account.isAdmin
-    }
-  }
+}
 
 
 // new Date() , new Array()
 function logValue(x: Date | string) {
   if (x instanceof Date) { // instanceof check the this object is intance of some class or something like that 
     console.log(x.toUTCString());
-    } else {
-        console.log(x.toUpperCase());
+  } else {
+    console.log(x.toUpperCase());
   }
 }
 
 
 
 
-type Fish = {swim:()=> void};
-type Bird = {fly:()=>void};
+type Fish = { swim: () => void };
+type Bird = { fly: () => void };
 
 function isFish(pet: Fish | Bird): pet is Fish { // 
-    return (pet as Fish).swim !== undefined;
+  return (pet as Fish).swim !== undefined;
 }
 
-function getFood(pet:Fish | Bird){
-    if(isFish(pet)){
-        pet
-        return "fish food"
-    }else{
-        pet 
-        return "bird Food"
-    }
+function getFood(pet: Fish | Bird) {
+  if (isFish(pet)) {
+    pet
+    return "fish food"
+  } else {
+    pet
+    return "bird Food"
+  }
 }
 
-interface Circle{
-    kind:"circle",
-    radius:number
+interface Circle {
+  kind: "circle",
+  radius: number
 }
 
 interface Square {
-    kind:"square",
-    side:number
+  kind: "square",
+  side: number
 }
 
 interface Rectangle {
-    kind:"rectangle",
-    length:number,
-    width:number
+  kind: "rectangle",
+  length: number,
+  width: number
 }
 
 //------------------
+// type guard
 
 type Shape = Circle | Square | Rectangle
 
-function getTrueShape(shape:Shape){
-    if(shape.kind === "circle"){
-        return Math.PI * shape.radius ** 2
-    }
-    // return shape.side * shape.side
+
+function getTrueShape(shape: Shape) {
+  if (shape.kind === "circle") {
+    return Math.PI * shape.radius ** 2
+  }
+  // return shape.side * shape.side
 }
 
-function getArea(shape:Shape){
-    switch(shape.kind){
-        case "circle":
-            return Math.PI * shape.radius ** 2
+function getArea(shape: Shape) {
+  switch (shape.kind) {
+    case "circle":
+      return Math.PI * shape.radius ** 2
 
-        case "square":
-            return shape.side * shape.side
-        
-        case "rectangle":
-            return shape.length*shape.width
-        
-        default:
-            const _defaultforshape:never = shape
-            return _defaultforshape
-    }
+    case "square":
+      return shape.side * shape.side
+
+    case "rectangle":
+      return shape.length * shape.width
+
+    default:
+      const _defaultforshape: never = shape
+      return _defaultforshape
+  }
 }
