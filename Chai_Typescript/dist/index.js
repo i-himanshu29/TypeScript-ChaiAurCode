381b94a9adea4cc96f6d9ea5ce44308a01249b34
Himanshu Maurya <mauryahimanshu979@gmail.com>
2026-01-01 21:26:09 +0530

    Basic Setup of TypeScript

---
+++ Chai_Typescript/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(name) {
+    return `Hello, ${name}!, welcome to TypeScript.`;
+}
+const userName = "Chai Aur TypeScript";
+console.log(greet(userName));
+//# sourceMappingURL=index.js.map
---
+++ Chai_Typescript/src/index.ts
@@ -0,0 +1,6 @@
+function greet(name: string): string {
+    return `Hello, ${name}!, welcome to TypeScript.`;
+}
+
+const userName: string = "Chai Aur TypeScript";
+console.log(greet(userName));
