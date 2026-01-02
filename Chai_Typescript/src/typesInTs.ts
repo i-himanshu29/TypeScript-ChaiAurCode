6705cbbf2cfbad03726bb5ea9d002ad9dbf8c4e5
Himanshu Maurya <mauryahimanshu979@gmail.com>
2026-01-02 21:56:06 +0530

    types , union and any in ts

--- Chai_Typescript/src/typesInTs.ts
+++ Chai_Typescript/src/typesInTs.ts
@@ -1,3 +1,12 @@
 let drink = "chai"
-
+// TypeScript infers the type of 'drink' as string
 let cups = Math.random() > 0.5 ? 10 : 5
+
+let channelName = "ChaiAurCode"
+// channelName = 22 // Error: Type 'number' is not assignable to type 'string'
+
+// Type annotation
+let chaiFlavour: string = "Masala Chai" 
+chaiFlavour = "Ginger Chai"
+
+let chaiOrder:boolean = true
