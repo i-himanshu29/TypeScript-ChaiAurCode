f871c3eff66a01ec93308644489413856b86b7f0
Himanshu Maurya <mauryahimanshu979@gmail.com>
2026-01-09 23:59:40 +0530

    renaming file name and adding good comment

--- Chai_Typescript/src/08_functionTs.ts
+++ Chai_Typescript/src/08_functionTs.ts
@@ -1,14 +1,18 @@
+// function with typescript...
+
 function makeChai(type: string, cups: number) {
     console.log(`Making a cup of ${type} with ${cups} cups.`);
 }
 
 makeChai("Masala Chai", 2);
 
+//.............function with return type.............
 function getChaiPrice(): number {
     // return "25 Ruppes"; // Error: Type 'string' is not assignable to type 'number'.
     return 50;
 }
 
+//.............function with parameter type and return type.............
 // function makeOrder(order:string): string{
 //     if(!order) return null; // Error: Type 'null' is not assignable to type 'string'.
 //     return order
@@ -21,16 +25,19 @@ function makeOrder(order: string) {
     return order;
 }
 
+// logger function without parameter and return type...
 function logChai(): void {
     console.log("Chai is being prepared...");
 }
-
+// // function with optional parameter...
 // function orderchai(type?:string){}
 
+// function with default parameter...
 function orderChai(type: string = "Masala") {
 
 }
 
+// function with object as parameter...
 function createChai(order: {
     type: string,
     sugar: number
--- Chai_Typescript/src/09_ArrayEnum.ts
+++ Chai_Typescript/src/09_ArrayEnum.ts
@@ -1,24 +1,33 @@
 // Arrays : Arrays are used to store multiple values in a single variable.
 
+//............Basic Array Type..........
 const chaiFlavours: string[] = ["Masala", "Ginger", "Elaichi"];
 const chaiPrice: number[] = [20, 25, 30];
 
+
+//..........Generic Array Type..............
 const ratings: Array<number> = [4.5, 4.0, 3.5, 5.0];
 type Chai = {
     type: string,
     price: number
 }
 
+
+//...........Array of Objects............
 const menu: Chai[] = [
     { type: "Masala", price: 20 },
     { type: "Ginger", price: 25 },
     { type: "Elaichi", price: 30 }
 ]
 
+
+//..........Readonly Arrays.................
 const cities: readonly string[] = ["Delhi", "Mumbai", "Chennai"]
 // cities.push("Kolkata") // Error: Property 'push' does not exist on type 'readonly string[]'.
 // cities[0]="New Delhi" // Error: Index signature in type 'readonly string[]' only permits reading.
 
+
+//..............Multi-dimensional Arrays..............
 const table: number[][] = [
     [1, 2, 3],
     [4, 5, 6],
@@ -27,20 +36,32 @@ const table: number[][] = [
 
 // Tuples: Tuples are a special type of array that allows you to store multiple values of different types in a single variable.
 
+//.............Basic Tuple Type.................
 let chaiTuple: [string, number, boolean];
 chaiTuple = ["Masala", 20, true];
 // chaiTuple = [20,"masala",true] // Error: Type 'number' is not assignable to type 'string'.
 
+
+//.............Optional Tuple Elements.................
 let userInfo: [string, number, boolean?]
 userInfo = ["John", 30]
 userInfo = ["Jane", 25, true]
 
+
+//.............Readonly Tuples.................
 const location: readonly [number, number] = [28.6139, 77.2090]
 
 const chaiItems: [name: string, price: number] = ["Ginger Chai", 25]
 
-// Enums : Enums are a way to define a set of named constants.
 
+
+// Enums : Enums are a way to define a set of named constants.
+// It makes it easier to document intent or create a set of distinct cases.
+// It can be only used with a set of related constants, such as days of the week, directions, etc.
+// single data type with multiple values.
+
+
+//.............Basic Enum Type.................
 enum ChaiSize {
     Small = "SMALL",
     Medium = "MEDIUM",
@@ -49,45 +70,58 @@ enum ChaiSize {
 
 // or 
 
+
+//.............Numeric Enum Type.................
 enum CupSize {
     SMALL,
     MEDIUM,
     LARGE
 }
 
-
 const size = CupSize.LARGE
 
+
+//.............Enum with Custom Numeric Values.................
 enum Status {
     PENDING = 100,
     SERVED, //101
     CANCELLED //102
 }
 
+
+//.............String Enum Type.................
 enum ChaiType {
     MASALA = "masala",
     GINGER = "ginger",
     ELAICHI = "elaichi"
 }
 
+
+//.............Using Enums in Functions.................
 function makeChai(type: ChaiType) {
     console.log(`Making a cup of ${type} chai.`);
 }
 makeChai(ChaiType.ELAICHI);
 // makeChai("masala");// masala is not assignable to parameter of type 'ChaiType'
 
+
+//.............Heterogeneous Enums.................
 enum RandomEnum {
     ID = 1,
     NAME = "Chai",
 }
 
+
+//.............Const Enums.................
 const enum Sugars {
     LOW = 1,
     MEDIUM = 2,
     HIGH = 3
 }
-
+// Usage of const enum
 const s = Sugars.HIGH;
 
+
+//.............Tuple with Enum.................
 let t: [String, number] = ["Test", 123];
 t.push("extra"); // This is allowed
